1282939659452f311b4a6cebe2686640a0ab26c6
Joe <relicorb@gmail.com>
2018-04-12 22:53:00 -0400

    Initial Commit

---
+++ app/src/main/java/com/ciphra/android/criminalintent/Crime.java
@@ -0,0 +1,52 @@
+package com.ciphra.android.criminalintent;
+
+import java.util.Date;
+import java.util.UUID;
+
+/**
+ * Created by Ciphra on 4/12/18.
+ */
+
+public class Crime {
+
+    private UUID mId;
+
+
+
+    private String mTitle;
+    private Date mDate;
+    private boolean mSolved;
+
+    public Crime(){
+        mId = UUID.randomUUID();
+        mDate = new Date();
+    }
+
+    public UUID getId() {
+        return mId;
+    }
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public void setTitle(String title) {
+        mTitle = title;
+    }
+
+    public Date getDate() {
+        return mDate;
+    }
+
+    public void setDate(Date date) {
+        mDate = date;
+    }
+
+    public boolean isSolved() {
+        return mSolved;
+    }
+
+    public void setSolved(boolean solved) {
+        mSolved = solved;
+    }
+}
---
+++ app/src/main/java/com/ciphra/android/criminalintent/CrimeFragment.java
@@ -0,0 +1,28 @@
+package com.ciphra.android.criminalintent;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Ciphra on 4/12/18.
+ */
+
+public class CrimeFragment extends Fragment {
+    private Crime mCrime;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        mCrime = new Crime();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+        View v = inflater.inflate(R.layout.fragment_crime, container, false);
+        return v;
+    }
+
+}
