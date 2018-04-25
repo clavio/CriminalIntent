dada030cad757b9811af68b5e4f9d4a9c6f2b142
6M21JH2\jbarra <relicorb@gmail.com>
2018-04-25 11:34:34 -0400

    datepicker is done (36)

--- app/src/main/java/com/ciphra/android/criminalintent/CrimeFragment.java
+++ app/src/main/java/com/ciphra/android/criminalintent/CrimeFragment.java
@@ -1,5 +1,7 @@
 package com.ciphra.android.criminalintent;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
@@ -13,6 +15,7 @@ import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.EditText;
 
+import java.util.Date;
 import java.util.UUID;
 
 /**
@@ -62,7 +65,7 @@ public class CrimeFragment extends Fragment {
         });
 
         mDateButton = (Button) v.findViewById(R.id.crime_date);
-        mDateButton.setText(mCrime.getDate().toString());
+        updateDate();
         mDateButton.setOnClickListener(new View.OnClickListener(){
 
             @Override
@@ -86,6 +89,24 @@ public class CrimeFragment extends Fragment {
         return v;
     }
 
+
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data){
+        if(resultCode != Activity.RESULT_OK){
+            return;
+        }
+        if(requestCode == REQUEST_DATE){
+            Date date = (Date) data.getSerializableExtra(DatePickerFragment.EXTRA_DATE);
+            mCrime.setDate(date);
+            updateDate();
+        }
+
+    }
+
+    private void updateDate() {
+        mDateButton.setText(mCrime.getDate().toString());
+    }
 
     public static CrimeFragment newInstance(UUID crimeId){
         Bundle args = new Bundle();
--- app/src/main/java/com/ciphra/android/criminalintent/CrimeListFragment.java
+++ app/src/main/java/com/ciphra/android/criminalintent/CrimeListFragment.java
@@ -1,5 +1,6 @@
 package com.ciphra.android.criminalintent;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -105,6 +106,9 @@ public class CrimeListFragment extends Fragment {
 
         return view;
     }
+
+
+
 
 
     @Override
