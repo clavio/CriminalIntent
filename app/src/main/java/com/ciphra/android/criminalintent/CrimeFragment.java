0532c0d48d139bb51d9277c55547c1b1648d556d
Joe <relicorb@gmail.com>
2018-04-25 08:10:38 -0400

    Fragment pushes (35)

--- app/src/main/java/com/ciphra/android/criminalintent/CrimeFragment.java
+++ app/src/main/java/com/ciphra/android/criminalintent/CrimeFragment.java
@@ -26,6 +26,8 @@ public class CrimeFragment extends Fragment {
     private CheckBox mSolvedCheckBox;
 
     private static final String ARG_CRIME_ID = "crime_id";
+
+    private static final int REQUEST_DATE = 0;
     private static final String DIALOG_DATE = "DialogDate";
 
     @Override
@@ -67,6 +69,7 @@ public class CrimeFragment extends Fragment {
             public void onClick(View v) {
                 FragmentManager manager = getFragmentManager();
                 DatePickerFragment dialog = DatePickerFragment.newInstance(mCrime.getDate());
+                dialog.setTargetFragment(CrimeFragment.this, REQUEST_DATE);
                 dialog.show(manager, DIALOG_DATE);
             }
         });
